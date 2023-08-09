1b9428d5de534398d8a14c1092a22e62cd178ac3
Ukpeh Michael <marcelukpeh@gmail.com>
2023-08-09 10:18:51 +0100

    Done with chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
+mod processes;
 mod state;
 mod todo;
 
-use serde_json::{json, value::Value, Map};
-use state::{read_file, write_to_file};
+use serde_json::{value::Value, Map};
+use state::read_file;
 use std::env;
 
 use todo::enums::TaskStatus;
 use todo::todo_factory;
 
-use todo::traits::{delete::Delete, edit::Edit, get::Get};
-use todo::ItemTypes;
+use crate::processes::process_input;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let status: &String = &args[1];
+    let command: &String = &args[1];
     let title: &String = &args[2];
 
-    let mut state: Map<String, Value> = read_file("./state.json");
-    println!("Before operation: {:?}", state);
-    state.insert(title.to_string(), json!(status));
-    println!("After operation: {:?}", state);
-    write_to_file("./state.json", &mut state);
+    let state: Map<String, Value> = read_file("./state.json");
+    let status: String;
+
+    match &state.get(*&title) {
+        Some(result) => status = result.to_string().replace('\"', ""),
+        None => {
+            status = "pending".to_owned();
+        }
+    }
+    let item = todo_factory(title, TaskStatus::from_string(status.to_uppercase()));
+    process_input(item, command.to_string(), &state);
 }
