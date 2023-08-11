34c765945d31dbe185f018d6f45b909aa599ab34
Ukpeh Michael <marcelukpeh@gmail.com>
2023-08-11 14:18:32 +0100

    Chapter 3 done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod processes;
 mod state;
 mod todo;
+mod views;
 
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use serde_json::{value::Value, Map};
 use state::read_file;
 use std::env;
@@ -10,20 +12,30 @@ use todo::enums::TaskStatus;
 use todo::todo_factory;
 
 use crate::processes::process_input;
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command: &String = &args[1];
-    let title: &String = &args[2];
 
-    let state: Map<String, Value> = read_file("./state.json");
-    let status: String;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let app = App::new().configure(views::views_factory);
+        return app;
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 
-    match &state.get(*&title) {
-        Some(result) => status = result.to_string().replace('\"', ""),
-        None => {
-            status = "pending".to_owned();
-        }
-    }
-    let item = todo_factory(title, TaskStatus::from_string(status.to_uppercase()));
-    process_input(item, command.to_string(), &state);
+    // let args: Vec<String> = env::args().collect();
+    // let command: &String = &args[1];
+    // let title: &String = &args[2];
+
+    // let state: Map<String, Value> = read_file("./state.json");
+    // let status: String;
+
+    // match &state.get(*&title) {
+    //     Some(result) => status = result.to_string().replace('\"', ""),
+    //     None => {
+    //         status = "pending".to_owned();
+    //     }
+    // }
+    // let item = todo_factory(title, TaskStatus::from_string(status.to_uppercase()));
+    // process_input(item, command.to_string(), &state);
 }
