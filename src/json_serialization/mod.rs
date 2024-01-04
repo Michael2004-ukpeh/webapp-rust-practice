2e32a48679c131b7c8ae997a5604e08eefeb465c
Ukpeh Michael <marcelukpeh@gmail.com>
2024-01-04 01:42:38 +0100

    Chapter 7 done without frontend

--- src/json_serialization/mod.rs
+++ src/json_serialization/mod.rs
@@ -2,3 +2,4 @@ pub mod todo_item;
 pub mod todo_items;
 pub mod new_user;
 pub mod login;
+pub mod login_response;
--- src/views/auth/login.rs
+++ src/views/auth/login.rs
@@ -1,4 +1,5 @@
 use crate::diesel;
+use crate::json_serialization::login_response::LoginResponse;
 use diesel::prelude::*;
 use actix_web::{web, HttpResponse, Responder};
 
@@ -8,8 +9,9 @@ use crate::json_serialization::login::Login;
 use crate::schema::users::{self};
 use crate::jwt::JwToken;
 use std::collections::HashMap;
+use std::os::macos::raw;
 
-pub async fn login(credentials:web::Json<Login>, mut db: DB) -> impl Responder{
+pub async fn login(credentials:web::Json<Login>, mut db: DB) -> HttpResponse{
 let password = credentials.password.clone();
 let users = users::table.filter(users::columns::username.eq(credentials.username.clone())).load::<User>(&mut db.connection).unwrap();
 if users.len() == 0{
@@ -21,12 +23,15 @@ match  users[0].verify(password) {
     true =>{
         let token =   JwToken::new(users[0].id);
         let raw_token = token.encode();
-        let mut body= HashMap::new();
-        body.insert("token",raw_token);
-        HttpResponse::Ok().json(body)
+        let response = LoginResponse{
+            token:raw_token.clone()
+        };
+        let body =serde_json::to_string(&response).unwrap();
+      
+        HttpResponse::Ok().append_header(("token", raw_token)).json(body)
     },
     false =>{
-        HttpResponse::Unauthorized().into()
+        HttpResponse::Unauthorized().finish()
     }
     
 }
